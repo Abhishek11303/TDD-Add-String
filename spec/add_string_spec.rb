c6002ef505ff702f36823bed2af02c17158eff8f
Abhishek <jaiswalabhishek0802@gmail.com>
2024-05-01 15:16:54 +0530

    test case for invalid string

--- spec/add_string_spec.rb
+++ spec/add_string_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe AddString, type: :request do
     it 'checks if method is present or not' do
       expect(AddString.method_defined?(:add_string)).to eq(true)
     end
+
+    it 'checks the string parameter is valid or not' do
+      object = AddString.new
+      response = object.add_string("1, \n")
+      expect(response).to eq("Invalid String")
+    end
   end
 end
