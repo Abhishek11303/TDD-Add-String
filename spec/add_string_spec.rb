a89237c52e75a59a0005979232dbbb5daff10754
Abhishek <jaiswalabhishek0802@gmail.com>
2024-05-01 13:58:06 +0530

    test case to check class is present or not

--- spec/add_string_spec.rb
+++ spec/add_string_spec.rb
@@ -1,4 +1,9 @@
 require './add_string'
 RSpec.describe AddString, type: :request do
-
+  describe 'Test cases for class AddString' do
+    it 'check wether class is present or not' do
+      object = AddString.new
+      expect(object).to be_kind_of(AddString)
+    end
+  end
 end
